00055fad62874efa36bc728669e963ddc16ce53a
Ankit yadav <ay870421@gmail.com>
2025-09-15 01:59:04 +0530

    category route added2

--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createCategory, getCategories } from "../Controllers/categoryController.js";
-import { protect } from "../middlewares/authMiddleware.js";
+import  protect  from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
